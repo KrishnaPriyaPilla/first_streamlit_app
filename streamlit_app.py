bff2d976b1cdaf189d8ae050b8a3e4b0b81b1acf
KrishnaPriyaPilla <97220562+KrishnaPriyaPilla@users.noreply.github.com>
2022-07-28 21:59:56 +0530

    Update streamlit_app.py
    Added emojis and last dish

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit
 
 streamlit.title ('My Parents New Healthy Diner')
 streamlit.header ('Breakfast Menu')
-streamlit.text ('Omega 3 and blueberry oatmeal')
-streamlit.text ('Kale, Spinach and Rocket smoothie')
-streamlit.text ('Hard-Boiled free-range egg')
+streamlit.text ('🥣 Omega 3 and blueberry oatmeal')
+streamlit.text ('🥗 Kale, Spinach and Rocket smoothie')
+streamlit.text ('🐔 Hard-Boiled free-range egg')
+streamlit.text ('🥑🍞 Avaocado toast')
