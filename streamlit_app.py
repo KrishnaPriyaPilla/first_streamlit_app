8aebc68525ba744edd795295298add7415104fa3
KrishnaPriyaPilla <97220562+KrishnaPriyaPilla@users.noreply.github.com>
2022-07-31 18:37:29 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -73,7 +73,3 @@ if streamlit.button('Add a fruit to the list'):
     my_cnx.close()
     streamlit.text(back_from_function)
 
-add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
-streamlit.write('Thanks for adding ', add_my_fruit)
-my_cur.execute("insert into FRUIT_LOAD_LIST values ('from streamlit')")
-
