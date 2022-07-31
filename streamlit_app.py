85ac207d8605479ab1e0a68a77ee527f58c44c2a
KrishnaPriyaPilla <97220562+KrishnaPriyaPilla@users.noreply.github.com>
2022-07-31 17:53:51 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,21 @@
 import streamlit
+import pandas
+import requests
+import snowflake.connector
+import urllib.error import URLError
 
+
+#import streamlit
 streamlit.title ('My Parents New Healthy Diner')
 streamlit.header ('Breakfast Menu')
 streamlit.text ('🥣 Omega 3 and blueberry oatmeal')
 streamlit.text ('🥗 Kale, Spinach and Rocket smoothie')
 streamlit.text ('🐔 Hard-Boiled free-range egg')
 streamlit.text ('🥑🍞 Avaocado toast')
-
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 
-import pandas
+#import pandas
 my_fruits_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruits_list = my_fruits_list.set_index('Fruit')
 # Let's put a pick list here so they can pick the fruit they want to include 
@@ -22,7 +27,8 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
-import requests
+
+#import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 #streamlit.text(fruityvice_response.json()) #just writes the data to the screen
 # take the json version of the response and normalize it
@@ -32,7 +38,7 @@ streamlit.dataframe(fruityvice_normalized)
 
 
 streamlit.stop()
-import snowflake.connector
+#import snowflake.connector
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("insert into FRUIT_LOAD_LIST values ('from streamlit')")
